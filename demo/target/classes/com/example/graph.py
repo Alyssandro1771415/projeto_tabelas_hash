77409493e9d3a0ae1157ee333866d6d6be469799
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2024-06-17 09:39:40 -0300

    Feat: Saving the graphs in a directory

--- demo/target/classes/com/example/graph.py
+++ demo/target/classes/com/example/graph.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 
 file_path = "./hash_table_results.csv"
 
-# Carregar os dados do CSV
 df = pd.read_csv(file_path)
 
-# Verificar se o DataFrame foi carregado corretamente
 if df.empty:
     raise ValueError(f"O arquivo CSV em {file_path} está vazio ou não pôde ser carregado corretamente.")
 
-# Separar os dados por Dataset
 df_100000 = df[df['Dataset'] == '100000 Unique']
 df_300000 = df[df['Dataset'] == '300000 Repeated']
 
-# Função para criar gráficos separados
+output_dir = "Graphs"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 def plot_individual_graph(df, dataset_label, metric, ylabel, color):
     plt.figure(figsize=(10, 6))
     plt.bar(df['HashTable'], df[metric], color=color)
@@ -24,17 +24,15 @@ def plot_individual_graph(df, dataset_label, metric, ylabel, color):
     plt.ylabel(ylabel)
     plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
-    plt.show()
+    plt.savefig(os.path.join(output_dir, f'{metric}_{dataset_label}.png'))
+    plt.close()
 
-# Função para chamar a criação dos gráficos
 def plot_graphs(df, dataset_label):
     plot_individual_graph(df, dataset_label, 'InsertTime', 'Tempo de Inserção (ms)', 'blue')
     plot_individual_graph(df, dataset_label, 'SearchTime', 'Tempo de Busca (ms)', 'green')
     plot_individual_graph(df, dataset_label, 'Collisions', 'Quantidade de Colisões', 'red')
     plot_individual_graph(df, dataset_label, 'Rehashes', 'Quantidade de Rehashes', 'purple')
 
-# Gerar gráficos para 100000 Unique
 plot_graphs(df_100000, "100000 Unique")
 
-# Gerar gráficos para 300000 Repeated
 plot_graphs(df_300000, "300000 Repeated")
